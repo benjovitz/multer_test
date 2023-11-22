b4568c2cfaace5d6f2358595d8a514804f4207f8
benjovitz <dbenjovitz@gmail.com>
2023-11-22 21:51:33 +0100

    billede send

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ app.get("/",  (req, res) => {
     res.sendFile(path.resolve("./index.html"))
 })
 
+app.get("/image", (req, res) => {
+    const blob = path.resolve("./uploads/363741c45dbf10ec9630c30d41b5ae67")
+    res.contentType("png")
+    res.sendFile(blob)
+})
+
 app.post("/profile", upload.single("avatar"), (req, res) => {
 
     res.end()
 })
+
 
 app.listen(8080)
