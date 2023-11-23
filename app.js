29a8bb650aab9904adfb86d14a209f7f64c2566a
benjovitz <dbenjovitz@gmail.com>
2023-11-23 03:42:08 +0100

    billeded

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 import express from "express"
 import path from "path"
+
+//__________________________________________
 import multer from "multer"
 
-const upload = multer({dest: "./uploads"})
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, './uploads')
+    },
+    filename: function (req, file, cb) {
+      console.log(file)
+      const parts = file.originalname.split(".")
+      const ext = parts[parts.length - 1]
+      //gem billede streng for user
+      cb(null, req.params.id + "." + ext)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+//__________________________________________
 const app = express()
 
 
@@ -12,13 +28,13 @@ app.get("/",  (req, res) => {
 })
 
 app.get("/image", (req, res) => {
-    const blob = path.resolve("./uploads/363741c45dbf10ec9630c30d41b5ae67")
-    res.contentType("png")
+    //find user på id og tag billede streng fra user
+    const blob = path.resolve("./uploads/1.png")
     res.sendFile(blob)
 })
 
-app.post("/profile", upload.single("avatar"), (req, res) => {
-
+app.post("/upload/:id", upload.single("file"), (req, res) => {
+    console.log(req.file)
     res.end()
 })
 
